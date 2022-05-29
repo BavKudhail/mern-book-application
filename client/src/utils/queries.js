2d987d294e8891a0f643ab4b8dae768e561df64c
BavKudhail <93915846+BavKudhail@users.noreply.github.com>
2022-05-29 17:47:55 +0100

    created client side query and mutations.js file

---
+++ client/src/utils/queries.js
@@ -0,0 +1,20 @@
+// * `queries.js`: This will hold the query `GET_ME`, which will execute the `me` query set up using Apollo Server.
+
+import { gql } from "@apollo/client";
+
+export const GET_ME = gql`
+  query me {
+    _id
+    username
+    email
+    bookCount
+    savedBooks {
+      bookId
+      authors
+      image
+      link
+      title
+      description
+    }
+  }
+`;
