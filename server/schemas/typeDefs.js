4ea737a1198594a509526fe51872a21cfa58f1ef
BavKudhail <93915846+BavKudhail@users.noreply.github.com>
2022-05-29 15:23:31 +0100

    created typeDef user

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,23 +1,40 @@
 const { gql } = require("apollo-server-express");
-// Notes: 
+// Notes:
 // a trailing ! = Not Null
 
 const typeDefs = gql`
-type Query {
-    me: User
-}
-type Mutation {
+  type Mutation {
     # Accepts an email and password as parameters; returns an Auth type.
     login(email: String!, password: String!): Auth
     # Accepts a username, email, and password as parameters; returns an Auth type
-    addUser(username: String!, email: String!, password: String! ): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
     # Accepts a book author's array, description, title, bookId, image, and link as parameters; returns a User type.
     saveBook(bookData: BookInput): User
     # removeBook: Accepts a book's bookId as a parameter; returns a User type
     removeBook(bookId: ID!): User
-}
-
+  }
+  type User {
+    _id: ID!
+    username: String
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
+  }
+  type Query {
+    me: User
+  }
 `;
 
 module.exports = typeDefs;
 
+//  `User` type:
+
+// 			* `_id`
+
+// 			* `username`
+
+// 			* `email`
+
+// 			* `bookCount`
+
+// 			* `savedBooks` (This will be an array of the `Book` type.)
