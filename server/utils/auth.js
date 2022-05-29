3c701170c95603880569932653f32f41158423a1
BavKudhail <93915846+BavKudhail@users.noreply.github.com>
2022-05-29 15:07:00 +0100

    created schema folder and file structure

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -10,20 +10,21 @@ module.exports = {
     // allows token to be sent via req.body, req.query, or headers
     let token = req.body.token || req.query.token || req.headers.authorization;
 
-    // ["Bearer", "<tokenvalue>"]
     if (req.headers.authorization) {
       token = token.split(" ").pop().trim();
     }
 
+    // if there is no token return the request object
     if (!token) {
       return req;
     }
 
-    // verify token and get user data out of it
+    // else verify the token and get user data out of it
     try {
       const { data } = jwt.verify(token, secret, { maxAge: expiration });
       req.user = data;
     } catch {
+      // if an error occurs...
       console.log("Invalid token");
     }
 
